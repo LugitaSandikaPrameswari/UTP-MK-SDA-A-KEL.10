e0638c9c19b76d271e3253ba7b7bc3dac96fe86b
AndriaLarasRamadhania <112565681+AndriaLarasRamadhania@users.noreply.github.com>
2023-04-12 22:22:26 +0700

    Update ubah.py
    Signed-off-by: AndriaLarasRamadhania <112565681+AndriaLarasRamadhania@users.noreply.github.com>

--- ubah.py
+++ ubah.py
@@ -11,27 +11,29 @@ def ubah():
                 print ('Ubah Data'.center(40))
                 print('=====================================')
 
-                
                 list_nama[j-1] = str(input("Masukan Nama Siswa     : "))
+
                 list_kelas[j-1] = str(input("Masukan Kelas          : "))
+                
                 nilai_mtk[j-1] = float(input("Masukan Nilai Mtk      : "))
+                
                 nilai_bindo[j-1] = float(input("Masukan Nilai Bindo    : "))
+                
                 nilai_ipa[j-1] = float(input("Masukan Nilai Ipa      : "))
+                
                 nilai_bing[j-1] = float(input("Masukan Nilai Bing     : "))
                 
-
                 print('=====================================')
                 print ('Data Tersimpan'.center(40))
                 print('=====================================\n')
                 
                 lihat = input ('\nTekan [enter] untuk melihat data ')
-                
                 tampil()
             else :
-              kembali = input("\nData tidak ada / Input Data terlebih dahulu [enter] ") 
+              kembali = input("\nData tidak ditemukan/Data tidak ada [enter] ") 
               menu()
 
         except (IndexError,ValueError):
-            kembali = input("\nData tidak ada / Input Data terlebih dahulu [enter] ") 
+            kembali = input("\nInputan Tidak Sesuai! [enter] ") 
             menu()
     rubah()
